2c90ef086c4ac80c64d03bff2836e44488d1ac03
unknown <admin@DTO-112-new5.interrao.ru>
2022-06-24 16:01:12 +0300

    added CRUD

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import mongoose from 'mongoose'
 
-import { loginValidation, registerValidation } from './validations.js'
+import { loginValidation, postCreateValidation, registerValidation } from './validations.js'
 import checkAuth from './utils/checkAuth.js'
 import { getMe, login, register } from './controllers/UserController.js'
+import * as PostController from './controllers/PostController.js'
 
 const app = express()
 
@@ -23,10 +24,12 @@ const PORT = 4444
 app.use(express.json())
 
 app.post('/auth/register', registerValidation, register)
-
 app.post('/auth/login', loginValidation, login)
-
 app.get('/auth/me', checkAuth, getMe)
+
+app.post('/posts', checkAuth, postCreateValidation, PostController.create)
+app.get('/posts', checkAuth, postCreateValidation, PostController.getAll)
+app.get('/posts/:id', checkAuth, postCreateValidation, PostController.getOne)
 
 app.listen(PORT, (err) => {
   if (err) {
