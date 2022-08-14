dc7995595114cd39dff6df29ec03dbc291b6c0ff
Maxim <maxtrekusovsite@gmail.com>
2022-08-14 22:43:24 +0300

    add enter on site

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,16 +1,5 @@
 import PostModel from '../models/Post.js'
 
-export const getAll = async (req, res) => {
-  try {
-    const posts = await PostModel.find().populate('user').exec()
-    res.json(posts)
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({
-      message: 'Не удалось получить статьи',
-    })
-  }
-}
 export const getLastTags = async (req, res) => {
   try {
     const posts = await PostModel.find().limit(5).exec()
@@ -18,7 +7,19 @@ export const getLastTags = async (req, res) => {
       .map((obj) => obj.tags)
       .flat()
       .slice(0, 5)
+
     res.json(tags)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'Не удалось получить статьи',
+    })
+  }
+}
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate('user').exec()
+    res.json(posts)
   } catch (err) {
     console.log(err)
     res.status(500).json({
